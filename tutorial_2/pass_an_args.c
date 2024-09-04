429537210d72df06fdf8391abcb18ba1e949d7af
thedeveloperyug <thedeveloperyug@gmail.com>
2024-09-04 14:52:18 +0530

    Passing arguments to the funtions

--- tutorial_2/pass_an_args.c
+++ tutorial_2/pass_an_args.c
@@ -1,68 +1,91 @@
-#include<linux/init.h>
-#include<linux/module.h>
-#include<linux/kernel.h>
-#include<linux/moduleparam.h>
-
-
-int value, arr_value[4];
-char *name;
-int cb_value=0;
-
-
-
-module_param(value,int,S_IRUSR | S_IWUSR);   //int value
-module_param(name,charp, S_IRUSR | S_IWUSR);  // String
-module_param_array(arr_value, int, NULL, S_IRUSR | S_IWUSR); //Array Of integer
+/***************************************************************************/ /**
+ * @file pass_an_args.c
+ * @author Yogesh (thedeveloperyug@gmail.com)
+ * @brief passing arguments to the function by cmd_line 
+ * @version 1.0
+ * @copyright Copyright (c) 2024
+ * 
+ */
+//******************************************************************************/
 
 
 
-// Module_param_callback
-int notify_param(const char* value, const struct kernel_param *kp){
-    int res = param_set_int(value,kp);
-    if(res==0){
-        printk(KERN_INFO "New value of cb_value = %d\n ", cb_value);
-        return 0;
+#include <linux/init.h>       
+#include <linux/module.h>      
+#include <linux/kernel.h>    
+#include <linux/moduleparam.h> 
+
+// Declare global module parameters
+int value, arr_value[4]; // Integer and an array of integers
+char *name;              // String for storing a name
+int cb_value = 0;        // Integer parameter with a default value
+
+// Define the parameters for the module
+
+// Module parameter for an integer 'value' with read and write permissions for the user
+module_param(value, int, S_IRUSR | S_IWUSR); // 'S_IRUSR' for read permission, 'S_IWUSR' for write permission
+
+// Module parameter for a string 'name' with read and write permissions for the user
+module_param(name, charp, S_IRUSR | S_IWUSR); // 'charp' type for string; 'S_IRUSR' and 'S_IWUSR' for read and write
+
+// Module parameter for an array of integers 'arr_value' with read and write permissions for the user
+module_param_array(arr_value, int, NULL, S_IRUSR | S_IWUSR); // 'NULL' indicates no maximum number of elements; 'S_IRUSR' and 'S_IWUSR' for read and write
+
+// Callback function for handling changes to 'cb_value'
+int notify_param(const char *value, const struct kernel_param *kp)
+{
+    int res = param_set_int(value, kp); // Set the parameter using the helper function for integers
+    if (res == 0)
+    {
+        printk(KERN_INFO "New value of cb_value = %d\n", cb_value); // Log the new value
+        return 0;                                                   // Success
     }
-    return -1;
-
+    return -1; // Failure
 }
 
+// Define operations for getting and setting the parameter
 const struct kernel_param_ops my_param_ops = {
-    .set = &notify_param,
-    .get = &param_get_int,
+    .set = &notify_param,  // Use our custom setter function
+    .get = &param_get_int, // Use the standard getter function for integers
 };
 
-module_param_cb(cb_value,&my_param_ops,&cb_value, S_IRUGO | S_IWUSR);
+// Register the callback function for 'cb_value' parameter
+module_param_cb(cb_value, &my_param_ops, &cb_value, S_IRUGO | S_IWUSR); // 'S_IRUGO' for read permission for all users; 'S_IWUSR' for write permission for the user
 
-
-static int __init pass_an_args_init(void){
+// Initialization function for the module
+static int __init pass_an_args_init(void)
+{
     int i;
-    int size = sizeof(arr_value)/sizeof(int); 
-    printk(KERN_INFO "Value = %d\n",value);
-    printk(KERN_INFO "CB_Value = %d\n",cb_value);
-    printk(KERN_INFO "Name  = %s  \n",name);
+    int size = sizeof(arr_value) / sizeof(int);
 
+    // Log the values of the module parameters
+    printk(KERN_INFO "Value = %d\n", value);       
+    printk(KERN_INFO "CB_Value = %d\n", cb_value); 
+    printk(KERN_INFO "Name = %s\n", name);      
 
-    for(i=0; i<size; i++){
-        printk(KERN_INFO " Arr_Value[%d] = %d\n",i, arr_value[i]);
+    // Print the elements of the integer array
+    for (i = 0; i < size; i++)
+    {
+        printk(KERN_INFO "Arr_Value[%d] = %d\n", i, arr_value[i]); // Print each element in the array
     }
 
-    printk(KERN_INFO " kERNEL MODULE LOADED SUCCESSFULLY...\n");
-    return 0;
-}   
-
-
-
-static void __exit pass_an_args_exit(void){
- printk(KERN_INFO " kERNAL MODULE Removed SUCCESSFULLY...\n");
-    
+    // Log that the module has been loaded successfully
+    printk(KERN_INFO "KERNEL MODULE LOADED SUCCESSFULLY...\n");
+    return 0; 
 }
 
-module_init(pass_an_args_init);
-module_exit(pass_an_args_exit);
+// Cleanup function for the module
+static void __exit pass_an_args_exit(void)
+{
+    // Log that the module has been removed successfully
+    printk(KERN_INFO "KERNEL MODULE REMOVED SUCCESSFULLY...\n");
+}
 
+// Register the initialization and cleanup functions
+module_init(pass_an_args_init); // Register the initialization function
+module_exit(pass_an_args_exit); // Register the cleanup function
 
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("thedeveloperyug");
-MODULE_VERSION("1.0");
+// Define module information
+MODULE_LICENSE("GPL");           
+MODULE_AUTHOR("thedeveloperyug"); 
+MODULE_VERSION("1.0");          
